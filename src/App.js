70baf4baf03d6ffb0f3f0116a3fb8321e2af648c
LatheWard <72397286+LatheWard@users.noreply.github.com>
2023-09-11 22:43:01 -0500

    Using useEffect to call const searchMovies, json in console

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ const API_URL = 'http://omdbapi.com/apikey=529a4538'
 
 const App = () => {
     
-        const searchMovies = async (title) =>{
+        const searchMovies = async (title) => {
             const response = await fetch(`${API_URL}&s=${title}`);
-            
+            const data = await response.json()
+
+            console.log(data);
         }
+
+        useEffect(() => {
+            searchMovies('');
+        }, []);
 
     return (
         <h1>App</h1>
