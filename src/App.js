b6a7d8d105a7359c8734744edbf9392c62f6eb7d
LatheWard <72397286+LatheWard@users.noreply.github.com>
2023-09-11 22:26:50 -0500

    init index.js, using useEffect + omdb for sample data

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {useEffect} from 'react'
+
+//529a4538
+
+const API_URL = 'http://omdbapi.com/apikey=529a4538'
+
+const App = () => {
+
+    useEffect(() => {
+        const searchMovies = async (title) =>{
+            const response = await fetch(`${API_URL}&s=${title}`)
+        }
+    })
+
+    return (
+        <h1>App</h1>
+    );
+}
+
+export default App;
